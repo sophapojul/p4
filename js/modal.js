ab04020309a07a36ecf858f1aecf8af34a85b294
Pascal Allau <pascal.allau@etu.toulouse-inp.fr>
2022-05-02 23:53:59 +0200

    feat: attempt keyboard access

--- js/modal.js
+++ js/modal.js
@@ -9,44 +9,38 @@ function editNav() {
 }
 
 // DOM Elements
-const modalBg = document.querySelector('.bground');
 const modalBtn = document.querySelectorAll('.modal-btn');
 const formData = document.querySelectorAll('.formData');
-const modalClose = document.querySelector('.close');
 const reserve = document.querySelector('#reserve');
 const first = document.querySelector('#first');
 const last = document.querySelector('#last');
 const email = document.querySelector('#email');
 const birthdate = document.querySelector('#birthdate');
 const quantity = document.querySelector('#quantity');
-const inputs = [first, last, email, birthdate, quantity];
-const radios = reserve.location;
-const locations = [
-  'New York',
-  'San Francisco',
-  'Seattle',
-  'Chicago',
-  'Boston',
-  'Portland'
-];
-const checkboxes = reserve.checkbox;
+const radioGroup = document.querySelector('#formRadio');
+const checkboxGroup = document.querySelector('#formMentions');
+const locations = document.querySelectorAll('input[name="location"]');
 
 const firstErrMsg = 'Prénom doit contenir au moins 2 caractères alphabétiques';
 const lastErrMsg = 'Nom doit contenir au moins 2 caractères alphabétiques';
-const emailErrMsg = 'Email doit être un email au format X@X.X';
-const birthdateErrMsg = 'Birthdate doit être une date valide, avant 2013';
-const quantityErrMsg = 'Vous devez saisir un nombre entier';
+const emailErrMsg =
+  'Email doit être au format X@X.xx(x) où X contient plusieurs caractères';
+const birthdateErrMsg =
+  'Birthdate doit être une date valide, inférieure à 2013';
+const quantityErrMsg = 'Vous devez saisir un nombre entier inférieur à 100';
 const radiosErrMsg = 'Vous devez choisir une ville';
+const cguErrMsg = 'Vous devez avoir lu et accepté les cgu';
 
 const firstRegExp = /^[-a-zA-Z']{2,20}$/i;
-const lastRegExp = /^[-a-zA-Z']{2,20}$/i;
+const lastRegExp = /^[-a-zA-Z'\s]{2,20}$/i;
 const emailRegExp = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.[a-z]{2,3})+$/i;
 const birthdateRegExp = /^(190[0-9]|19[1-9][0-9]|200[0-9]|201[0-2])/;
-const quantityRegExp = /^\d+$/;
+const quantityRegExp = /^(0?\d|[1-9]\d)$/;
 
 function setErrMsg(elt, message) {
+  const eltLastChild = elt.parentElement.lastChild;
   // test présence d'un message d'erreur
-  if (elt.parentElement.lastChild.nodeName === 'SMALL') {
+  if (eltLastChild.nodeName === 'SMALL') {
     return;
   } // sinon création du message d'erreur :
   // création de l'élément contenant le message
@@ -59,10 +53,11 @@ function setErrMsg(elt, message) {
 }
 
 function removeErrMsg(elt) {
+  const eltLastChild = elt.parentElement.lastChild;
   // test présence d'un message d'erreur
-  if (elt.parentElement.lastChild.nodeName === 'SMALL') {
+  if (eltLastChild.nodeName === 'SMALL') {
     // suppression du message d'erreur
-    elt.parentElement.lastChild.remove();
+    eltLastChild.remove();
     // remplacement de la classe invalid par la classe valid sur le parent
     elt.parentElement.classList.remove('invalid');
     elt.parentElement.classList.add('valid');
@@ -70,10 +65,8 @@ function removeErrMsg(elt) {
 }
 
 function testRegExp(value, regExp) {
-  if (regExp.test(value)) {
-    return true;
-  }
-  return false;
+  const testedValue = regExp.test(value);
+  return !!testedValue;
 }
 
 function valid(elt, errMsg, regExp) {
@@ -85,93 +78,135 @@ function valid(elt, errMsg, regExp) {
       return true;
     }
     setErrMsg(elt, errMsg);
-    elt.focus();
     return false;
   }
   return false;
 }
 
+function notNull(data) {
+  return !!data.value;
+}
+
+function notEmpty(data) {
+  const dataLength = data.value.length;
+  return dataLength > 0;
+}
+
 function firstValid() {
-  if (valid(first, firstErrMsg, firstRegExp)) {
-    return true;
+  if (!notNull(first)) {
+    return false;
+  }
+  if (notEmpty(first)) {
+    return valid(first, firstErrMsg, firstRegExp);
   }
   return false;
 }
 
 function lastValid() {
-  if (valid(last, lastErrMsg, lastRegExp)) {
-    return true;
+  if (!notNull(last)) {
+    return false;
+  }
+  if (notEmpty(last)) {
+    return valid(last, lastErrMsg, lastRegExp);
   }
   return false;
 }
 
 function emailValid() {
-  if (valid(email, emailErrMsg, emailRegExp)) {
-    return true;
+  if (!notNull(email)) {
+    return false;
+  }
+  if (notEmpty(email)) {
+    return valid(email, emailErrMsg, emailRegExp);
   }
   return false;
 }
 
 function birthdateValid() {
-  if (valid(birthdate, birthdateErrMsg, birthdateRegExp)) {
-    return true;
+  const toTestDate = new Date(birthdate.value);
+  if (!notNull(birthdate)) {
+    return false;
+  }
+  if (notEmpty(birthdate)) {
+    if (
+      birthdate.value.length !== 10 ||
+      toTestDate.toString() === 'Invalid Date'
+    ) {
+      setErrMsg(birthdate, birthdateErrMsg);
+
+      return false;
+    }
+    // if (toTestDate.getFullYear() < 2012)
+    return valid(birthdate, birthdateErrMsg, birthdateRegExp);
   }
   return false;
 }
 
 function quantityValid() {
-  if (valid(quantity, quantityErrMsg, quantityRegExp)) {
-    return true;
+  if (!notNull(quantity)) {
+    return false;
   }
+  if (notEmpty(quantity))
+    if (valid(quantity, quantityErrMsg, quantityRegExp)) {
+      return true;
+    }
   return false;
 }
 
 function isSelected() {
-  if (Array.from(radios).filter((elt) => elt.checked).length) {
-    if (reserve.children['6'].nodeName === 'SMALL') {
-      reserve.children['6'].remove();
-      return true;
+  const selected = document.querySelector('input[name="location"]:checked');
+  if (selected) {
+    if (document.querySelector('#formRadio').nextSibling.nodeName === 'SMALL') {
+      document.querySelector('#formRadio').nextSibling.remove();
     }
     return true;
   }
-  if (reserve.children['6'].nodeName !== 'SMALL') {
+  if (document.querySelector('#formRadio').nextSibling.nodeName !== 'SMALL') {
     const errElt = document.createElement('small');
     errElt.textContent = radiosErrMsg;
-    reserve.children['5'].insertAdjacentElement('afterend', errElt);
-    reserve.children['6'].style.fontSize = '1rem';
-    reserve.children['6'].style.color = 'red';
+    document
+      .querySelector('#formRadio')
+      .insertAdjacentElement('afterend', errElt);
+    document.querySelector('#formRadio').nextSibling.style.fontSize = '1rem';
+    document.querySelector('#formRadio').nextSibling.style.color = 'red';
+    return false;
   }
-  return false;
+  return selected;
 }
-
-function radiosValid() {
-  radios.forEach((radio) => radio.addEventListener('change', isSelected));
-  return isSelected();
+function cguChecked() {
+  const cgu = document.querySelector('#checkbox1');
+  if (!cgu.checked) {
+    setErrMsg(cgu, cguErrMsg);
+    return false;
+  }
+  removeErrMsg(cgu);
+  return cgu.checked;
 }
-
 function validate() {
-  if (
+  const validated = !!(
     firstValid() &&
     lastValid() &&
-    emailValid &&
+    emailValid() &&
     birthdateValid() &&
     quantityValid() &&
     isSelected() &&
-    radiosValid
-  ) {
-    return true;
-  }
-  return false;
+    cguChecked()
+  );
+  return validated;
 }
-reserve.addEventListener(
-  'submit',
-  (e) => {
-    if (!validate()) {
-      e.preventDefault();
-    }
-  },
-  false
-);
+reserve.addEventListener('submit', (ev) => {
+  ev.preventDefault();
+  validate();
+  // if (validate()) {
+  //   window.location = './';
+  // }
+});
+// const inputs = document.querySelectorAll('change');
+// for (let i = 0; i < inputs.length; i += 1) {
+//   inputs[i].addEventListener('change', () => {
+//     valid(this);
+//   });
+// }
 first.addEventListener('input', () => {
   firstValid();
 });
@@ -187,113 +222,154 @@ birthdate.addEventListener('input', () => {
 quantity.addEventListener('input', () => {
   quantityValid();
 });
-
+radioGroup.addEventListener('input', () => {
+  isSelected();
+});
+checkboxGroup.addEventListener('input', () => {
+  cguChecked();
+});
 document.addEventListener('DOMContentLoaded', () => {
   const triggers = document.querySelectorAll('[aria-haspopup="dialog"]');
-  const doc = document.querySelector('main');
-  const focusableElementsArray = [
-    'input:not([disabled])',
-    'select:not([disabled])',
-    '[tabindex]:not([tabindex="-1"])'
-  ];
+  const modalWrapper = document.querySelectorAll('.modal');
+
+  const modalForm = document.getElementById('dialog');
+  const modalContent = document.querySelector('.modal-content');
+  const modalClose = document.querySelector('.modal-close');
+  const modalBody = document.querySelector('.modal-body');
+  let modal = null;
+  const focusableSelector = 'input, span, button';
+  let focusableElements = [];
+  // const doc = document.querySelector('main');
+  // const focusableElementsArray = [
+  //   'input:not([disabled])',
+  //   'select:not([disabled])',
+  //   '[tabindex]:not([tabindex="-1"])'
+  // ];
+  function stopPropagation(e) {
+    e.stopPropagation();
+  }
 
   // close modal
-  const closeModal = (modal, trig) => {
-    doc.setAttribute('aria-hidden', false);
-    modalBg.style.display = 'none';
-    modal.removeAttribute('aria-modal');
-    modal.setAttribute('aria-hidden', true);
-
+  const closeModal = (e) => {
+    e.preventDefault();
+    modalForm.style.display = 'none';
+    modalForm.setAttribute('aria-hidden', true);
+    modalForm.removeAttribute('aria-modal');
+    modalForm.removeEventListener('click', closeModal);
+    modalClose.removeEventListener('click', closeModal);
+    modalContent.removeEventListener('click', stopPropagation);
+    modal = null;
     // restoring focus
-    trig.focus();
+    // trig.focus();
   };
 
-  const openModal = (modal) => {
-    const focusableElements = modal.querySelectorAll(focusableElementsArray);
-    const firstFocusableElement = focusableElements[0];
-    const lastFocusableElement =
-      focusableElements[focusableElements.length - 1];
+  const openModal = (e) => {
+    e.preventDefault();
+    focusableElements = Array.from(
+      modalBody.querySelectorAll(focusableSelector)
+    );
+    console.log(focusableElements);
+    // const firstFocusableElement = focusableElements[0];
+    // const lastFocusableElement =
+    //   focusableElements[focusableElements.length - 1];
 
-    doc.setAttribute('aria-hidden', true);
-    modalBg.style.display = 'block';
-    modal.setAttribute('aria-hidden', false);
-    modal.setAttribute('aria-modal', true);
-
+    // doc.setAttribute('aria-hidden', true);
+    modalForm.style.display = 'block';
+    modalForm.removeAttribute('aria-hidden');
+    modalForm.setAttribute('aria-modal', true);
+    modalForm.addEventListener('click', closeModal);
+    modalClose.addEventListener('click', closeModal);
+    modalContent.addEventListener('click', stopPropagation);
+    modal = modalForm;
     // return if no focusable element
-    if (!firstFocusableElement) {
-      return;
+    // if (!firstFocusableElement) {
+    //   return;
+    // }
+
+    // window.setTimeout(() => {
+    //   firstFocusableElement.focus();
+
+    //   // trapping focus inside the dialog
+    //   focusableElements.forEach((focusableElement) => {
+    //     if (focusableElement.addEventListener) {
+    //       focusableElement.addEventListener('keydown', (event) => {
+    //         const tab = event.code === 'Tab';
+
+    //         if (!tab) {
+    //           return;
+    //         }
+
+    //         if (event.shiftKey) {
+    //           if (event.target === firstFocusableElement) {
+    //             // shift + tab
+    //             event.preventDefault();
+
+    //             lastFocusableElement.focus();
+    //           }
+    //         } else if (event.target === lastFocusableElement) {
+    //           // tab
+    //           event.preventDefault();
+
+    //           firstFocusableElement.focus();
+    //         }
+    //       });
+    //     }
+    //   });
+    // }, 100);
+  };
+
+  const blockFocusInModal = (e) => {
+    e.preventDefault();
+    let index = focusableElements.findIndex(
+      (f) => f === modalForm.querySelector(':focus')
+    );
+    index += 1;
+    if (index >= focusableElements.length) {
+      index = 0;
     }
-
-    window.setTimeout(() => {
-      firstFocusableElement.focus();
-
-      // trapping focus inside the dialog
-      focusableElements.forEach((focusableElement) => {
-        if (focusableElement.addEventListener) {
-          focusableElement.addEventListener('keydown', (event) => {
-            const tab = event.code === 'Tab';
-
-            if (!tab) {
-              return;
-            }
-
-            if (event.shiftKey) {
-              if (event.target === firstFocusableElement) {
-                // shift + tab
-                event.preventDefault();
-
-                lastFocusableElement.focus();
-              }
-            } else if (event.target === lastFocusableElement) {
-              // tab
-              event.preventDefault();
-
-              firstFocusableElement.focus();
-            }
-          });
-        }
-      });
-    }, 100);
+    focusableElements[index].focus();
   };
 
   triggers.forEach((trigger) => {
-    const dialog = document.getElementById('dialog');
-    const dismissTriggers = dialog.querySelectorAll('[data-dismiss]');
-
-    // open modal
-    trigger.addEventListener('click', (event) => {
-      event.preventDefault();
-
-      openModal(dialog);
-    });
-
-    trigger.addEventListener('keydown', (event) => {
-      if (event.code === 'Enter') {
-        event.preventDefault();
-        openModal(dialog);
-      }
-    });
-
-    // close modal
-    dismissTriggers.forEach((dismissTrigger) => {
-      const dismissModal = document.getElementById('dismissModal');
-
-      dismissTrigger.addEventListener('click', (event) => {
-        event.preventDefault();
-        closeModal(dismissModal);
-      });
-    });
-
-    dialog.addEventListener('keydown', (event) => {
-      if (event.code === 'Escape') {
-        closeModal(dialog, trigger);
-      }
-    });
-
-    window.addEventListener('click', (event) => {
-      if (event.target === dialog) {
-        closeModal(dialog);
-      }
-    });
+    trigger.addEventListener('click', openModal);
+    //   const dialog = document.getElementById('dialog');
+    //   const dismissTriggers = dialog.querySelectorAll('[data-dismiss]');
+    //   // open modal
+    //   trigger.addEventListener('click', (event) => {
+    //     event.preventDefault();
+    //     openModal(dialog);
+    //   });
+    //   trigger.addEventListener('keydown', (event) => {
+    //     if (event.code === 'Enter') {
+    //       event.preventDefault();
+    //       openModal(dialog);
+    //     }
+    //   });
+    //   // close modal
+    //   dismissTriggers.forEach((dismissTrigger) => {
+    //     const dismissModal = document.getElementById('dismissModal');
+    //     dismissTrigger.addEventListener('click', (event) => {
+    //       event.preventDefault();
+    //       closeModal(dismissModal);
+    //     });
+    //   });
+    //   dialog.addEventListener('keydown', (event) => {
+    //     if (event.code === 'Escape') {
+    //       closeModal(dialog, trigger);
+    //     }
+    //   });
+    //   window.addEventListener('click', (event) => {
+    //     if (event.target === dialog) {
+    //       closeModal(dialog);
+    //     }
+    //   });
+  });
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+      closeModal(e);
+    }
+    if (e.key === 'Tab') {
+      blockFocusInModal(e);
+    }
   });
 });
